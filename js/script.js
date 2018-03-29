e9f110185d5203208e07d5f21ac6f02ecface505
Andrew Turnage <aturnage@hotmail.com>
2018-03-29 12:01:35 -0500

    Added about section on index, js, and css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Places the navi html into index
 $.get("navigation.html", function(data){
     $(".navigationBar").replaceWith(data);
+});
+// Places the about html into index
+$.get("about.html", function(data){
+    $(".about").replaceWith(data);
 });
